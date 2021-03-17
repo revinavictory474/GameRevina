91c464db9566a79a3828ff5ae3157763eb14a7f7
Victory <revina.victory@gmail.com>
2021-03-17 10:40:48 +0700

    V 0.2
    need add weather

---
+++ Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject fireball;
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    private bool alive;
+
+    private void Start()
+    {
+        alive = true;
+    }
+    private void Update()
+    {
+        if (alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (fireball == null)
+                    {
+                        fireball = Instantiate(fireballPrefab) as GameObject;
+                        fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+                
+        }
+    }
+
+    public void SetAlive(bool _alive)
+    {
+        alive = _alive;
+    }
+
+    
+}
